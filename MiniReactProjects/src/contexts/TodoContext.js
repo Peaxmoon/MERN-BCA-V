b79815edd73c11e8019651619d08868984ef015c
Peaxmoon <sujjalboi09@gmail.com>
2025-04-29 23:34:12 +0545

    context s

--- MiniReactProjects/src/contexts/TodoContext.js
+++ MiniReactProjects/src/contexts/TodoContext.js
@@ -1,11 +1,16 @@
 import { createContext, useContext } from "react";
 
 
-
-
-
-
-export const TodoContext = createContext();
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo Msg",
+            Completed: false,
+        },
+    ]   
+    theme: dark
+});
 
 export const useTodo = () => {
     return useContext(TodoContext);
